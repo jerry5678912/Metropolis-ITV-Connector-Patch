2b00d96be778fb720e0baf9b5cda17125e35bae5
jerry5678912 <203045957+jerry5678912@users.noreply.github.com>
2026-09-17 17:57:32 +0800

    Move main class to project namespace

---
+++ src/io/github/jerry5678912/metropolisitvpatch/MetropolisITVPatch.java
@@ -0,0 +1,10 @@
+package io.github.jerry5678912.metropolisitvpatch;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("metropolis_itv_patch")
+public final class MetropolisITVPatch {
+    public MetropolisITVPatch() {
+        System.out.println("[Metropolis ITV Patch] v1.1.0 loaded: Connector-safe visible ITV renderer enabled.");
+    }
+}
